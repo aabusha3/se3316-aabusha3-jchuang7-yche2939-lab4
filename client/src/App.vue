67d265eec0c0037bdd92148320acb72eca64e108
aabusha3 <aabusha3@uwo.ca>
2022-11-29 23:09:46 -0500

    re-add router-link

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,10 +10,16 @@ const getGenres = async function() {
     console.log(genres)
   }
 
-  getGenres();
+ // getGenres();
 </script>
 
 <template>
+      <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+
   <h1>test</h1>  
   <ul>
     <li v-for="genre of genres" :key="genre.id">
